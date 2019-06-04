73b863d07f8ac4bd630207d12271d7474652d0ad
Nicolas Cassis <nc@k30.no>
2019-06-04 02:24:29 +0200

    init

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+importScripts("/precache-manifest.4b16cf8611290b6a08f99422d453d740.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+self.addEventListener('message', (e) => {
+    if (!e.data) {
+      return;
+    }
+  
+    switch (e.data) {
+      case 'skipWaiting':
+        self.skipWaiting();
+        break;
+      default:
+        // NOOP
+        break;
+    }
+  });
+  
+  workbox.clientsClaim();
+  
+  // The precaching code provided by Workbox.
+  self.__precacheManifest = [].concat(self.__precacheManifest || []);
+  workbox.precaching.suppressWarnings();
+  workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
